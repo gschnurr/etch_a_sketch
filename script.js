799d4ade7af196c73adddb09b1957be9c2f6efdf
Serraphiim <gibschnurr@gmail.com>
2018-08-09 22:17:50 -0500

    most of the way done with the JS only a few minor changes and clean up needed. CSS is very basic but looks decent enough as a place holder. I might end up adding more end-user configuration of the playing area.

--- script.js
+++ script.js
@@ -1,65 +1,79 @@
 //this project will not be responsive
 // I need to double check my math and how I get this because it is only looking at gridWidth
-//right now which could be an issue
 // rgb white (255, 255, 255) rgb black (0, 0, 0) or I could use opacity and increment it
-//add button clicks for Large grid 6400, medium 1600, small 400
 
 
-let numberCells = 6400;
-let gridWidth = document.getElementById('gridBox').clientWidth;
-let gridArea = (gridWidth ** 2);
-let cellArea = (gridArea / numberCells);
-let cellSize = Math.sqrt(cellArea);
+let numberCells;
 let cells;
+let cellsArray;
 let grid = [];
 
 
-function createGrid() {
+function createGrid(numberCells) {
+  let gridWidth = document.getElementById('gridBox').clientWidth;
+  let gridArea = (gridWidth ** 2);
+  let cellArea = (gridArea / numberCells);
+  let cellSize = Math.sqrt(cellArea);
   for(let y = 0; y < numberCells; y++) {
     cells = document.createElement('div');
     //style cells section
-    cells.className = 'cells';
+    cells.className = 'cellsClass';
     cells.style.width = cellSize + 'px';
     cells.style.height = cellSize + 'px';
     gridBox.appendChild(cells);
     cells.addEventListener("mouseover", colorCells);
   }
 }
-createGrid();
 // the this statement is important because without it we are saying that the last
 // div created will own the function on any mouseover, however the this makes the
 // object that is being moused over own the function.
+// the + infront of the this statment returns the numeric expression of the object
+//not the object itself
 
 function removeGrid() {
-  cells = document.getElementsByClassName('cells');
-  for ()
-  //I will need to create a function that will remove the last div with a loop then
-  //rebuild it with the new variable.
-  //the removal function should be done with a for loop
-  //for(i=0; i > cells.length; i--) or something like this
-  //cells = getElementsByClassName(cells)
+  cellsArray = document.getElementsByClassName('cellsClass');
+  for (i = cellsArray.length; (i = cellsArray.length) && (i >= 0); i--) {
+    gridBox.removeChild(gridBox.firstChild);
+  }
 }
 
+
 function colorCells() {
-  this.style.backgroundColor = 'black';
+  let shade = +this.style.opacity;
+  if (shade < 1) {
+	  shade += 0.25;
+  }
+  this.style.opacity = shade;
 }
 
 function selectGridSize() {
-  largeButton.addEventListener('click', () => gridSize('large'));
+  largeButton.addEventListener('click', ()=> gridSize('large'));
+  mediumButton.addEventListener('click', ()=> gridSize('medium'));
+  smallButton.addEventListener('click', ()=> gridSize('small'));
 }
+
 
 
 function gridSize(userSelection) {
   switch (userSelection) {
     case 'large':
-      numberCells = 400;
-      removeGrid();
-      createGrid();
+	  removeGrid();
+      createGrid(6400);
       break;
+	case 'medium':
+	  removeGrid();
+	  createGrid(1600);
+	  break;
+	case 'small':
+	  removeGrid();
+	  createGrid(400);
+	  break;
     default:
-      alert('something is broke');
+      alert('oops...something went wrong!');
       break;
   }
 }
 
+window.onload = gridSize('small');
 selectGridSize();
+createGrid();
